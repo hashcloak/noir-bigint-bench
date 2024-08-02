b709c3591da5c45f9762dc047ecfac05f5440327
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2024-08-02 12:22:29 -0600

    WIP running 100x a multiplication in U256. The script won't run successfully for me.

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -15,27 +15,7 @@ main_file="src/main.nr"
 results_file="results/results_${experiment_name}.csv"
 
 declare -a limbs=(
-    230
-    250
-    470
-    590
-    710
-    830
-    950
-    1000
-    1070
-    1190
-    1310
-    1435
-    1555
-    1675
-    1790
-    1915
-    2035
-    2155
-    2048
-    3000
-    4000
+256
 )
 
 # Defines the header for the CSV file
@@ -44,8 +24,8 @@ echo "Bits,ACIR opcodes,Circuit size" > $results_file
 for n_bits in "${limbs[@]}"; do
     # Replace the bits in the source code and write it into the main.nr
     echo "Executing experiment for $n_bits"
-    replaced_code=$(sed "s/\${bits}/$n_bits/" $code_content_file)
-    echo "$replaced_code" > $main_file 
+    # replaced_code=$(sed "s/\${bits}/$n_bits/" $code_content_file)
+    # echo "$code_content_file" > $main_file 
 
     # Compile the file.
     nargo compile
