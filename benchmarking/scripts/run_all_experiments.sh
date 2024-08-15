b6db77822b340dfedc24d3f39e801d134c75b1b2
hdvanegasm <hdvanegasm@gmail.com>
2024-08-15 12:51:33 -0500

    feat: make the number of iterations variable

--- benchmarking/scripts/run_all_experiments.sh
+++ benchmarking/scripts/run_all_experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash ./scripts/bench_gate_number.sh "$1"
-bash ./scripts/bench_proving_time.sh  "$1"
-bash ./scripts/bench_verification_time.sh "$1"
+bash ./scripts/bench_gate_number.sh "$@"
+bash ./scripts/bench_proving_time.sh  "$@"
+bash ./scripts/bench_verification_time.sh "$@"
